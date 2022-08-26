f6f407ec3be0632ec0f8b9f624b2c8c5647d3af4
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-26 09:47:47 +0530

    [kavya] Add: Logical programs

---
+++ LogicalPrograms/LogicalPrograms/Fibbonaci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Fibbonaci
+    {
+        public static void Pro()
+        {
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int t1 = 0, t2 = 1, t3 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(t1);
+                }
+                if (i == 1)
+                {
+                    Console.WriteLine(t2);
+                }
+                t3 = t1 + t2;
+                t1 = t2;
+                t2 = t3;
+                Console.WriteLine(t3);
+            }
+        }
+    }
+}
